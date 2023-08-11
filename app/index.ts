0ffc2288e4a2c5a3cf69d9aa0f229d6da4adf6f3
Sihoon Kim <hoffnung1208@gmail.com>
2023-08-11 12:54:05 +0900

    minor change

--- app/index.ts
+++ app/index.ts
@@ -6,7 +6,7 @@ import { createApp } from "./app";
 const { PORT, REDIS_URL } = process.env;
 
 if (!PORT) throw new Error("PORT is required");
-if (!REDIS_URL) throw new Error("REDIS_URL is required");
+if (!REDIS_URL) throw new Error("REDIS_URL is required!");
 
 const startServer = async () => {
   const client = redis.createClient({ url: REDIS_URL });
